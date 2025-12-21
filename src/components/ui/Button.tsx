5612de8caf84a95d49be25f940169e918a3447f9
aadi-codeSs <adityasingh0999@outlook.com>
2025-12-21 17:38:02 +0530

    bootstrapping react project

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,12 +3,15 @@ export interface ButtonProps {
     variant: "primary" | "secondary";
     size: "sm" | "md" | "lg";
     text: string;
-    startIcon: any;
-    endIcon: any;
+    startIcon?: any;
+    endIcon?: any;
     onClick: () => void;
 }
 
-export const Button = () => {
+export const Button = ( props: ButtonProps ) => {
+
+    <div class = "  ddd "></div>
+
     return <button></button>
 
 }
